44d31d952cbb2cda3c9713ce5135243649ceae65
Tommy Han <tommy.han@salesforce.com>
2025-09-10 23:47:05 -0400

    alarmClock modifications

---
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import AlarmClockAssets from '@salesforce/resourceUrl/AlarmClockAssets';
+
+export default class AlarmClockApp extends LightningElement {
+
+    clockImage = AlarmClockAssets+'/AlarmClockAssets/clock.png';   
+}
