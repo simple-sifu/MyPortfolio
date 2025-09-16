fe5c853b649d603e6a2157f0bc6fc1f4036ace40
Tommy Han <tommy.han@salesforce.com>
2025-09-16 11:02:52 -0400

    add form

--- force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
+++ force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
@@ -1,11 +1,47 @@
+import Name from '@salesforce/schema/Account.Name';
 import { LightningElement } from 'lwc';
 
+const DEFAULT_NOTE_FORM = {
+    Name:"",
+    Note_Description__c:""
+}
 export default class NoteTakingApp extends LightningElement {
     showModal = false;
+    noteRecord = DEFAULT_NOTE_FORM;
+    formats = [
+        'font',
+        'size',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'list',
+        'indent',
+        'align',
+        'link',
+        'clean',
+        'table',
+        'header',
+        'color',
+    ];
     createNoteHandler(){
         this.showModal = true;
     }
     closeModalHandler(){
         this.showModal = false;
+        this.noteRecord = DEFAULT_NOTE_FORM;
+    }
+
+    changeHandler(event){
+        const { name, value } = event.target;
+        this.noteRecord={...this.noteRecord, [name]:value};
+    }
+    formSubmitHandler(event){
+        event.preventDefault();
+        console.log("noteRecord: ", JSON.stringify(this.noteRecord));
+    }
+
+    get isFormInvalid(){
+        return  !(this.noteRecord && this.noteRecord.Name !== "" && this.noteRecord.Note_Description__c === "");
     }
 }
