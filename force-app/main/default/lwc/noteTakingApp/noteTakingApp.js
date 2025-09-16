80de52bf526c41f83d4ab6f50d30ec81117d3d47
Tommy Han <tommy.han@salesforce.com>
2025-09-16 09:57:48 -0400

    add NoteTaking app

--- force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
+++ force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
 
-export default class NoteTakingApp extends LightningElement {}
+export default class NoteTakingApp extends LightningElement {
+    showModal = false;
+    createNoteHandler(){
+        this.showModal = true;
+    }
+    closeModalHandler(){
+        this.showModal = false;
+    }
+}
